99884113c981f277fafa9a8f532e35e1e3a62067
SerranoValenciaJuanDavid <serranovalenciajuandavid949@gmail.com>
2023-10-30 12:38:01 -0500

    feat: Creacion Clase Contexto

--- Infrastructure/Data/FarmaCampusContext.cs
+++ Infrastructure/Data/FarmaCampusContext.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class FarmaCampusContext:DbContext
-    {
-        
+    public class FarmaCampusContext : DbContext
+    {   
+        public FarmaCampusContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
